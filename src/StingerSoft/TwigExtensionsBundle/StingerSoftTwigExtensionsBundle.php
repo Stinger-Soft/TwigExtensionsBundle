9f0917987d6be324df366a0c397a5c69eb44049d
Florian Meyer <florian.meyer@project-engineers.de>
2016-08-01 14:23:43 +0200

    initial commit

---
+++ src/StingerSoft/TwigExtensionsBundle/StingerSoftTwigExtensionsBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Stinger Twig Extensions Bundle.
+ *
+ * (c) Oliver Kotte <oliver.kotte@stinger-soft.net>
+ * (c) Florian Meyer <florian.meyer@stinger-soft.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace StingerSoft\TwigExtensionsBundle;
+
+class StingerSoftTwigExtensionsBundle extends Bundle {
+	
+}
